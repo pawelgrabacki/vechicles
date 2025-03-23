97add7aa72d2b12e3bda1441ad2fb94fcea5c5f4
pawelgrabacki <paw.grabacki@gmail.com>
2025-03-23 13:54:42 +0100

    init

---
+++ ConsoleApp/Pojazd.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagram
+{
+    public abstract class Pojazd
+    {
+        public string Marka { get; set; }
+        public string TypSilnika { get; set; } // np. benzyna, diesel, elektryczny
+        public int MaksymalnaPrędkość { get; set; }
+        public double Spalanie { get; set; } // litry na 100 km
+        public double Ciężar { get; set; } // w kg
+        public decimal Cena { get; set; } // Cena pojazdu
+
+        public abstract void WyswietlInformacje();
+        public virtual decimal KosztPrzejazduNa100km(decimal cenaPaliwa)
+        {
+            return (decimal)Spalanie * cenaPaliwa;
+        }
+    }
+
+    public class SamochodOsobowy : Pojazd
+    {
+        public int IloscDrzwi { get; set; }
+        public bool Szyberdach { get; set; }
+        public bool CzyMozeCiagnacPrzyczepke { get; set; }
+
+        public override void WyswietlInformacje()
+        {
+            Console.WriteLine($"Samochód osobowy: {Marka}, Typ silnika: {TypSilnika}, Maks. prędkość: {MaksymalnaPrędkość} km/h, Spalanie: {Spalanie} l/100km, Ciężar: {Ciężar} kg, Cena: {Cena}, Drzwi: {IloscDrzwi}, Szyberdach: {Szyberdach}, Przyczepka: {CzyMozeCiagnacPrzyczepke}");
+        }
+    }
+
+    public class Motocykl : Pojazd
+    {
+        public bool MaKufry { get; set; } // Czy motocykl jest wyposażony w kufry
+
+        public override void WyswietlInformacje()
+        {
+            Console.WriteLine($"Motocykl: {Marka}, Typ silnika: {TypSilnika}, Maks. prędkość: {MaksymalnaPrędkość} km/h, Spalanie: {Spalanie} l/100km, Ciężar: {Ciężar} kg, Cena: {Cena}, Kufry: {MaKufry}");
+        }
+    }
+
+    public class Ciezarowka : Pojazd
+    {
+        public double Ladownosc { get; set; } // Maksymalna ładowność
+
+        public override void WyswietlInformacje()
+        {
+            Console.WriteLine($"Ciężarówka: {Marka}, Typ silnika: {TypSilnika}, Maks. prędkość: {MaksymalnaPrędkość} km/h, Spalanie: {Spalanie} l/100km, Ciężar: {Ciężar} kg, Cena: {Cena}, Ładowność: {Ladownosc}");
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SamochodOsobowy samochod = new SamochodOsobowy
+            {
+                Marka = "Toyota",
+                TypSilnika = "benzyna",
+                MaksymalnaPrędkość = 220,
+                Spalanie = 7.5,
+                Ciężar = 1450,
+                Cena = 100000,
+                IloscDrzwi = 5,
+                Szyberdach = true,
+                CzyMozeCiagnacPrzyczepke = true
+            };
+
+            var motocykl = new Motocykl
+            {
+                Marka = "Yamaha",
+                TypSilnika = "benzyna",
+                MaksymalnaPrędkość = 180,
+                Spalanie = 5.0,
+                Ciężar = 250,
+                Cena = 45000,
+                MaKufry = true
+            };
+
+            var ciezarowka = new Ciezarowka
+            {
+                Marka = "Volvo",
+                TypSilnika = "diesel",
+                MaksymalnaPrędkość = 140,
+                Spalanie = 30.0,
+                Ciężar = 7500,
+                Cena = 300000,
+                Ladownosc = 24000
+            };
+
+            samochod.WyswietlInformacje();
+            motocykl.WyswietlInformacje();
+            ciezarowka.WyswietlInformacje();
+        }
+    }
+}
